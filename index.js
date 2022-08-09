9d60cdd28cbef3d1896aea217cd875d86240dd0f
Jake <jakepucci26@gmail.com>
2022-08-09 09:47:17 -0600

    commit tree e4b7fc1a77f7bc06e737b63357401cecadc17d4e

---
+++ index.js
@@ -0,0 +1,10 @@
+// var divde = function(){
+//     return 'bob';
+// }
+
+const divide = (param1, param2) => param1 / param2;
+divide(2000,100)
+
+const square = param1 => (param1) **2
+
+const add = (param1, param2) => param1 + param2
